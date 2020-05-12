9d64afa15ff080773069195d15b1614c18508731
Pawel <52802778+PawelKrzesinski@users.noreply.github.com>
2020-05-12 10:51:40 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const clear = document.querySelector("#clear");
+const date = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("itemInput");
+
+const check = "fa-check-circle";
+const uncheck = "fa-circle";
+const lineThrough = "lineThrough";
